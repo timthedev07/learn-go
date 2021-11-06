c4eab30427a262d4d4be78dee0a1e60d837d6cb2
Tim <harvard90873@gmail.com>
2021-11-06 20:33:02 +0100

    more basic stuff with go

--- basics.go
+++ basics.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// casting, using the math module, and printing decimals
+	x := 5
+	calcResult := math.Sin(1/(math.Pow(float64(x)+3.482847, 3))) * (180 / math.Pi)
+	fmt.Printf("%g\n", calcResult)
+	fmt.Printf("%F\n", calcResult)
+	fmt.Printf("%f\n", calcResult)
+	fmt.Printf("%.6f\n", calcResult)
+	fmt.Printf("%e\n", calcResult)
+
+	// format string
+	someStr := fmt.Sprintf("Calculation result: %f", calcResult)
+	someOtherStr := fmt.Sprintf("Calculation result to 4dp: %.4f", calcResult)
+	fmt.Println(someStr)
+	fmt.Println(someOtherStr)
+	fmt.Printf("\n\n")
+
+	// getting input
+	res := input("Your name: ")
+	fmt.Println(res)
+
 }
